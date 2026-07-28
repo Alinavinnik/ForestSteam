dd047cef49a78c9a8f67d376f2bf5b1d5728eca8
Alina <alinavinnikhgfff@gmail.com>
2026-07-28 14:44:28 +0300

    update html

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
-
+const openBtnEl = document.querySelector('[data-menu-open]');
+const closeBtnEl = document.querySelector('[data-menu-close]');
+const menuEl = document.querySelector('[data-menu]');
+const burgerEl = document.querySelector('[data-burger]');
+const menuLinksEl = document.querySelectorAll('[data-menu-link]');
