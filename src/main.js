17985123f916b2054dddf284cbb616e46999caa0
Alina <alinavinnikhgfff@gmail.com>
2026-07-30 15:36:01 +0300

    add js folder

--- src/main.js
+++ src/main.js
@@ -1,41 +1,5 @@
-const openBtnEl = document.querySelector('[data-menu-open]');
-const closeBtnEl = document.querySelector('[data-menu-close]');
-const menuEl = document.querySelector('[data-menu]');
-const burgerEl = document.querySelector('[data-burger]');
-const menuLinksEl = document.querySelectorAll('[data-menu-link]');
+import accordionHowToPlay from './js/accordion.how_to_play.js';
+import modalWindow from '/js/modal';
 
-openBtnEl.addEventListener('click', toggleMenu);
-closeBtnEl.addEventListener('click', toggleMenu);
-
-menuEl.addEventListener('click', e => {
-  if (e.target === menuEl) {
-    toggleMenu();
-  }
-});
-
-menuLinksEl.forEach(link => {
-  link.addEventListener('click', toggleMenu);
-});
-
-function toggleMenu() {
-  const isOpen = menuEl.dataset.visible === 'open';
-
-  menuEl.dataset.visible = isOpen ? 'close' : 'open';
-  burgerEl.dataset.visible = isOpen ? 'show' : 'hide';
-
-  document.body.classList.toggle('no-scroll', !isOpen);
-}
-
-// анімація бургера
-const hotspotEl = document.querySelector('[data-hero-hotspot]');
-const burgerAnimationEl = document.querySelector('[data-hero-burger]');
-
-if (hotspotEl && burgerAnimationEl) {
-  hotspotEl.addEventListener('mouseenter', () => {
-    burgerAnimationEl.dataset.animation = 'show';
-  });
-
-  hotspotEl.addEventListener('mouseleave', () => {
-    burgerAnimationEl.dataset.animation = 'hide';
-  });
-}
+modalWindow();
+accordionHowToPlay();
