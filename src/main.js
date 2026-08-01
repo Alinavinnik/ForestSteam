d2987d2a6819d52fba6990db1210559493721a16
Alina <alinavinnikhgfff@gmail.com>
2026-08-01 14:17:03 +0300

    add show google btn function

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import accordionHowToPlay from './js/accordion.how_to_play.js';
 import modalWindow from '/js/modal';
 import './js/reviews';
+import showGoogleBtn from './js/header.js';
 
 modalWindow();
 accordionHowToPlay();
+showGoogleBtn();
